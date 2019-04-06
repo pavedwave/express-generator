d1078cf23cd8d808f8d07a528c921e7208db910d
James Peters <pavedwave@gmail.com>
2019-04-06 10:01:55 -0700

    Pre-REST API

---
+++ routes/promoRouter.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const promoRouter = express.Router();
+
+promoRouter.use(bodyParser.json());
+
+// chained actions
+
+promoRouter.route('/')
+.all((req,res,next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req,res,next) => {
+    res.end('Sending promotions!');
+})
+.post((req, res, next) => {
+    res.end('Will add promotion: ' + req.body.name + ' with details: ' + req.body.description);
+})
+.put((req, res, next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /promos');
+})
+.delete((req, res, next) => {
+    res.end('Deleting all promotions...');
+});
+
+promoRouter.route('/:promoId')
+.all((req,res,next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req,res,next) => {
+    res.end('Sending promotions!');
+})
+.post((req, res, next) => {
+    res.end('Will add promotion: ' + req.body.name + ' with details: ' + req.body.description);
+})
+.put((req, res, next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /promos');
+})
+.delete((req, res, next) => {
+    res.end('Deleting all promotions...');
+});
+
+module.exports = promoRouter;
