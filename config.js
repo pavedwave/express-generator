9ef004f7a6be03a3e779978c56934bbaf52f2792
James Peters <pavedwave@gmail.com>
2019-04-19 19:50:46 -0700

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
-    'facebook': {
-        clientId: '427896857967942',
-        clientSecret: '8c49bf949a6e70fe6b8bdd4bd54221a7'
-    }
-}
